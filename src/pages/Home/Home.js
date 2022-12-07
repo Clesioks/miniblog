73bb338b0a4a2f104b6972e23db23f7829ef68c0
Clesioks <kornalewski@outlook.com>
2022-12-07 13:20:18 -0300

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Home.module.css';
 
 const Home = () => {
+  const [lista, setLista] = useState(['comida']);
+
+  let onRemove = (removeItem) => {
+    setLista((prev) => prev.filter((i) => i !== removeItem));
+  };
+
+  const handleSub = (e) => {
+    e.preventDefault();
+
+    setLista((prev) => [...prev, e.target.newItem.value]);
+  };
+
   return (
     <div>
       <h1>Home</h1>
+      <form onSubmit={handleSub}>
+        <input name="newItem" />
+      </form>
+      <ul>
+        {lista.map((item) => (
+          <li key={item}>
+            {item}
+            <button onClick={() => onRemove(item)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
